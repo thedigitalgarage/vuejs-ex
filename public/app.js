933b970faeb96e9fd024f9dc79afb92c0df25df5
thedigitalgarage <johnmccawley@outlook.com>
2017-03-03 14:00:09 -0700

    reorganize

---
+++ public/app.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World! Your first Vue.js Application on Digital Garage'
+    }
+});
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 
 // configuration =================
-app.use('/', express.static(__dirname)); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 app.use(morgan('combined')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json

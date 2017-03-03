a73b07ec8cb06b3c588d3aad9574423f6cdaa078
thedigitalgarage <johnmccawley@outlook.com>
2017-03-03 08:08:08 -0700

    remove mongoose

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var fs = require('fs')
 var mongoose = require('mongoose'); // mongoose for mongodb
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
-var database = require('./config/database'); //load the database config
 
 // configuration =================
 app.use('/', express.static(__dirname)); // set the static files location /public/img will be /img for users
@@ -18,10 +17,6 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse applicati
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-mongoose.connect(database.url);// connect to mongoDB database
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
 
 // listen (start app with node server.js) ======================================
 app.listen(port, ip);
